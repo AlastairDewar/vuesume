701cb5da5c7abd02da75a7496524c359a35bf751
Ally Dewar <Alastair@adewra.com>
2018-07-22 20:09:26 +0100

    Added images and lightbox

--- src/components/PortfolioComponent.vue
+++ src/components/PortfolioComponent.vue
@@ -1,24 +1,35 @@
 <template src="../../static/theme/base/portfolio.html">
 </template>
-
 <script>
 import TimelineComponent from './TimelineComponent'
+import LightBox from 'vue-image-lightbox'
+
 export default {
   computed: {
     portfolio () {
       return this.$store.getters.getSection('portfolio')
     },
-    template () {
-      return this.$store.getters.getTemplate('portfolio.html')
-    },
     employment () {
       return this.$store.getters.getSection('employment')
+    },
+    template () {
+      return this.$store.getters.getTemplate('portfolio.html')
     }
   },
-  components: {'TimelineComponent': TimelineComponent}
+  components: {'TimelineComponent': TimelineComponent, 'LightBox': LightBox},
+  methods: {
+    openGallery (index) {
+      this.$refs.lightbox[0].showImage(index)
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+    ul li img {
+        padding: 0 .125rem
+    }
+    ul li img:hover {
+        cursor: pointer;
+    }
 </style>
