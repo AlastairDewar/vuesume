b90774d24432a746f86ff3915c20b032968722a1
Ally Dewar <Alastair@adewra.com>
2018-08-04 14:40:56 +0100

    Added switch for Advanced Mode

--- src/components/ProfileComponent.vue
+++ src/components/ProfileComponent.vue
@@ -3,6 +3,7 @@
 
 <script>
 import LanguagesComponent from './LanguagesComponent'
+import Switches from 'vue-switches'
 
 export default {
   name: 'headerview',
@@ -11,12 +12,52 @@ export default {
       return this.$store.getters.getSection('profile')
     }
   },
-  components: {'LanguagesComponent': LanguagesComponent}
+  components: {'LanguagesComponent': LanguagesComponent, 'Switches': Switches},
+  data () {
+    return {
+      advancedMode: false
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style>
     div.profile p.header-text i.icon {
         margin-right: 0.5rem;
+    }
+    span.vue-switcher__label {
+        text-align: center;
+    }
+    label.vue-switcher {
+        width: 100%;
+    }
+
+
+    label.vue-switcher.vue-switcher--bold.vue-switcher-theme--custom.vue-switcher-color--white div {
+      background-color: #dcdcdc;
+      margin-left: auto;
+      margin-right: auto;
+    }
+    label.vue-switcher.vue-switcher--bold.vue-switcher-theme--custom.vue-switcher-color--white div:after {
+        background-color: #f2f2f2;
+    }
+    label.vue-switcher.vue-switcher--bold.vue-switcher-theme--custom.vue-switcher-color--white.vue-switcher--unchecked div {
+        background-color: #e8e8e8;
+    }
+    label.vue-switcher.vue-switcher--bold.vue-switcher-theme--custom.vue-switcher-color--white.vue-switcher--unchecked div:after {
+        background-color: #f5f5f5;
+    }
+    label.vue-switcher input {
+        top: 0;
+    }
+    .vue-switcher--bold .vue-switcher__label span {
+        font-family: 'Open Sans', sans-serif;
+        color: #ffffff;
+        font-size: 1.25rem;
+        font-weight: 400;
+        line-height: 1.25;
+    }
+    label.vue-switcher {
+        margin-bottom: 1rem;
     }
 </style>
