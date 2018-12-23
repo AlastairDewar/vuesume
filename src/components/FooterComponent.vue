a71340f3a6ea82526797b260484ac7be24e0fa76
Ally Dewar <Alastair@adewra.com>
2018-12-23 19:30:47 +0000

    Amended content

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,9 +1,12 @@
 <template>
-  <footer class="footer hero no-print">
+  <footer class="footer hero">
     <div class="container">
       <div class="content has-text-centered">
-        <p>
+        <p class="no-print">
           If you're interested in hiring me, then <a href="mailto:alastair@adewra.com">get in touch</a>.
+        </p>
+        <p class="print-only">
+          Please check out the interactive version I made with pictures and videos at <a href="https://cv.adewra.com/">https://cv.adewra.com</a>.
         </p>
       </div>
     </div>
